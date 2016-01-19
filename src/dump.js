1e746825399755ce5329d0639d1066607603376b
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-18 22:08:16 -0800

    Check for `__coverage__` before dumping.
    Avoid `undefined` errors when no files are instrumented.

--- src/dump.js
+++ src/dump.js
@@ -3,11 +3,9 @@ import { dirname, join } from 'path';
 import mkdirp from 'mkdirp';
 
 export default function dump({ coverage, path, pretty }) {
-  if (typeof coverage !== 'undefined') {
-    const file2 = join(path, 'coverage.json');
-    const data = pretty ?
-      JSON.stringify(coverage, null, '  ') : JSON.stringify(coverage);
-    mkdirp.sync(dirname(file2));
-    writeFileSync(file2, data);
-  }
+  const file2 = join(path, 'coverage.json');
+  const data = pretty ?
+    JSON.stringify(coverage, null, '  ') : JSON.stringify(coverage);
+  mkdirp.sync(dirname(file2));
+  writeFileSync(file2, data);
 }
